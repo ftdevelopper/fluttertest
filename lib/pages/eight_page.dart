bc138b1d0a48b1c24c53f8b38b4d39058f36e5c0
Tomas Tisocco <tomastisocco97@gmail.com>
2021-07-24 12:50:32 -0300

    Add animated_container

---
+++ lib/pages/eight_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EigthPage extends StatefulWidget {
+  EigthPage({Key? key}) : super(key: key);
+
+  @override
+  _EigthPageState createState() => _EigthPageState();
+}
+
+class _EigthPageState extends State<EigthPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: null,
+    );
+  }
+}
