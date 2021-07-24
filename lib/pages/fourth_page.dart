7d93850c07a1e0a85ebcf0be3bfff9dd4969521a
Tomas Tisocco <tomastisocco97@gmail.com>
2021-07-24 11:55:54 -0300

    Update fourth Page

--- lib/pages/fourth_page.dart
+++ lib/pages/fourth_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_print, prefer_typing_uninitialized_variables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_print, prefer_typing_uninitialized_variables, deprecated_member_use
 
 import 'package:flutter/material.dart';
 
