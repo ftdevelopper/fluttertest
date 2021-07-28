7ccd1500549eac4526d128d49a8f6ab503b18c11
Tomas Tisocco <tomastisocco97@gmail.com>
2021-07-28 13:14:31 -0300

    Update Stack Page add structure

--- lib/pages/stack_page.dart
+++ lib/pages/stack_page.dart
@@ -5,8 +5,22 @@ class StackPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stack Page')
+      ),
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: 7,
+          itemBuilder: (BuildContext context, int index){
+            if (index.isEven){
+              return const StackWidget();
+            } else {
+              return const StackFavoriteWidget();
+            }
+          },
+        )
+      ),
     );
   }
 }
