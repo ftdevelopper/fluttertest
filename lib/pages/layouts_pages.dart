951f8002ae92787de1a4d4e517d590d5f5467467
Tomas Tisocco <tomastisocco97@gmail.com>
2021-07-28 20:41:30 -0300

    Add Widgets to the Layout Page

--- lib/pages/layouts_pages.dart
+++ lib/pages/layouts_pages.dart
@@ -49,4 +49,126 @@ class LayoutPage extends StatelessWidget {
       ),
     );
   }
+
+  _buildJournalHeaderImage(){
+    return Image(
+      image: AssetImage('assets/present.jpg'),
+      fit: BoxFit.cover,
+    );
+  }
+
+  _buildJournalEntry(){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'My Birthday',
+          style: TextStyle(
+            fontSize: 32.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Divider(),
+        Text(
+          'Its going to be a great birthday, We are going out for dinner at my favorite place, then watch a movie after we go to the gelateria fo ice cream and espresso',
+          style: TextStyle(color: Colors.black54),
+        ),
+      ],
+    );
+  }
+
+  _buildJournalWeather(){
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Icon(Icons.wb_sunny,
+            color: Colors.orange,
+            )
+          ],
+        ),
+        SizedBox(width: 16.0,),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Text(
+                  '81º Clear',
+                  style: TextStyle(color: Colors.deepOrange),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Text(
+                  '4500 Sant Alpho Drive, Dallas, TX United States',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ]
+    );
+  }
+
+  _buildJournalTags(){
+    return Wrap(
+      spacing: 8.0,
+      children: List.generate(7, (int index){
+        return Chip(
+          label: Text(
+            'Gift ${index + 1}',
+            style: TextStyle(fontSize: 10.0),
+          ),
+          avatar: Icon(
+            Icons.card_giftcard,
+            color: Colors.blue.shade300,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(4.0),
+            side: BorderSide(color: Colors.grey),
+          ),
+          backgroundColor: Colors.grey.shade100,
+        );
+      }
+      )
+    );
+  }
+
+  _buildJournalFooterImages(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        CircleAvatar(
+          backgroundImage: AssetImage('assets/salmon.jpg'),
+          radius: 40.0,
+        ),
+        CircleAvatar(
+          backgroundImage: AssetImage('assets/asparagus.jpg'),
+          radius: 40.0,
+        ),
+        CircleAvatar(
+          backgroundImage: AssetImage('assets/strawberries.jpg'),
+          radius: 40.0,  
+        ),
+        SizedBox(
+          width: 100,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Icon(Icons.cake),
+              Icon(Icons.star_border),
+              Icon(Icons.music_note),
+              //Icon(Icons.movie),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
 }
