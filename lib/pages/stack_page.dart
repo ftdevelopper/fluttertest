0acec45cb45b737d45b50b66269b52ff514e5716
Tomas Tisocco <tomastisocco97@gmail.com>
2021-07-28 12:12:57 -0300

    Create Empty Stack Page

---
+++ lib/pages/stack_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class StackPage extends StatelessWidget {
+  const StackPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
