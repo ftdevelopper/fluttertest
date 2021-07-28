6187bba3f972a2356a2b7bb13358a8ea1131e8d9
Tomas Tisocco <tomastisocco97@gmail.com>
2021-07-28 19:17:42 -0300

    Update Layout Page - Add Structure

--- lib/pages/layouts_pages.dart
+++ lib/pages/layouts_pages.dart
@@ -5,8 +5,21 @@ class LayoutPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Layouts Page',
+          style: TextStyle(color: Colors.black87),
+        ),
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black87),
+        brightness: Brightness.light,
+        leading: IconButton(icon: Icon(Icons.menu), onPressed: (){}),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.cloud_queue), onPressed: (){}),
+        ],
+      ),
+      body: _buildBody(),
     );
   }
 }
