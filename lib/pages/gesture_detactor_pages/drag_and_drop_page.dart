258b00bcb2b6448401f4d361ec595004cff4c590
Tomas Tisocco <tomastisocco97@gmail.com>
2021-07-29 19:41:27 -0300

    Add Drag and Drop Widgets

--- lib/pages/gesture_detactor_pages/drag_and_drop_page.dart
+++ lib/pages/gesture_detactor_pages/drag_and_drop_page.dart
@@ -8,13 +8,16 @@ class DragAndDropPage extends StatefulWidget {
 }
 
 class _DragAndDropPageState extends State<DragAndDropPage> {
-  late String _gestureDetected;
+  late String _gestureDetected = '';
+  late Color _paintedColor = Colors.grey;
+  var acceptedData;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
+        title: Text('Gesture & Drgag an Drop'),
+        centerTitle: true,
       ),
       body: SafeArea(
         child: SingleChildScrollView(
@@ -25,9 +28,9 @@ class _DragAndDropPageState extends State<DragAndDropPage> {
                 color: Colors.black,
                 height: 44.0,
               ),
-
+              _buildDraggable(),
               Divider(height: 40.0),
-
+              _buildDragTarget(),
               Divider(color: Colors.black),
             ],
           ),
@@ -54,6 +57,22 @@ class _DragAndDropPageState extends State<DragAndDropPage> {
         print('inPanUpdate: $details');
         _displayGestureDetected('onPanUpdate: \n$details');
       },
+      //onVerticalDragUpdate: ((DragUpdateDetails details) {
+      //  print('onVerticalDragUpdate: $details');
+      //  _displayGestureDetected('onVerticalDragUpdate:\n$details');
+      //}),
+      //onHorizontalDragUpdate: (DragUpdateDetails details) {
+      //  print('onHorizontalDragUpdate: $details');
+      //  _displayGestureDetected('onHorizontalDragUpdate:\n$details');
+      //},
+      //onHorizontalDragEnd: (DragEndDetails details) {
+      //  print('onHorizontalDragEnd: $details');
+      //  if (details.primaryVelocity < 0) {
+      //    print('Dragged Right to Left: ${details.primaryVelocity}');
+      //  } else if (details.primaryVelocity > 0) {
+      //    print('Dragged Left to Right: ${details.primaryVelocity}');
+      //  }
+      //},
       child: Container(
         color: Colors.lightGreen.shade100,
         width: double.infinity,
@@ -71,21 +90,56 @@ class _DragAndDropPageState extends State<DragAndDropPage> {
     );
   }
 
-  @override
-  void initState() { 
-    super.initState();
-    _gestureDetected = "";
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-
-  }
-
   void _displayGestureDetected(String gesture){
     setState((){
       _gestureDetected = gesture;
     });
+  }
+
+  Draggable<int> _buildDraggable(){
+    return Draggable(
+      child: Column(
+        children: <Widget>[
+          Icon(Icons.palette,
+          color: Colors.deepOrange,
+          size: 48.0,
+          ),
+          Text(
+            'Drag Me bellow to change color'
+          ),
+        ],
+      ),
+      childWhenDragging: Icon(
+        Icons.palette,
+        color: Colors.grey,
+        size: 48.0,
+      ),
+      feedback: Icon(
+        Icons.brush,
+        color: Colors.deepOrange,
+        size: 80.0,
+      ),
+      data: Colors.deepOrange.value,
+    );
+  }
+
+  DragTarget<int> _buildDragTarget(){
+    return DragTarget<int>(
+      onAccept: (colorValue){
+        _paintedColor = Color(colorValue);
+      },
+      builder: (BuildContext context, List<dynamic> accpetedData, List<dynamic> rejectedData) => 
+        accpetedData.isEmpty ? Text(
+        'Drag To and see color change',
+        style: TextStyle(color: _paintedColor),
+        ) 
+        : Text(
+          'Painting Color: $acceptedData',
+          style: TextStyle(
+          color: Color(accpetedData[0]),
+          fontWeight: FontWeight.bold,
+        ),
+      )
+    );
   }
 }
